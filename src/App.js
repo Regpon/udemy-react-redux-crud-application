46dbf9d96a5dc0ba752640d08cd347afa451d931
Regpon <yuusuke@okamoto.us>
2018-12-01 19:17:44 +0900

    why jsx

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>
-          Hello, World!
-        </h1>
-      </div>
+      <h1>
+        Hello, World!
+      </h1>
     );
   }
 }
